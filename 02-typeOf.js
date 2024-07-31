44222610787a81162990f5f87601b458276ab788
Puribharat2002 <bharatpuri7189@gmail.com>
2024-07-31 21:11:16 +0530

    typeOf.js
    typeOf.js

---
+++ 02-typeOf.js
@@ -0,0 +1,48 @@
+console.log("=========== 1 =============");
+var bank="undefine";
+console.log("Bank Name Is:",bank);
+
+var bank="State Bank ";
+console.log("Bank Name Is:",bank);
+
+var bank="string";
+console.log("Bank Name DataType Is:",bank);
+
+console.log("============= 2 ================");
+
+var marks = "90 %";
+console.log("My Marks Are :",  marks);
+
+var marks ="string";
+console.log("DataType of Marks :", marks);
+
+console.log("============== 3 ================");
+
+var status="undefine";
+console.log("My working Status is:",status);
+
+var status="true";
+console.log("My Working Status is:", status);
+
+var status="boolean";
+console.log("Working DataType is: " ,status);
+
+console.log("============= 4 =================");
+
+console.log("========== a =========");
+
+var count=" Hundred And Seven ";
+console.log("Total count is : ", count);
+
+var count="string";
+console.log("Total Count DataType is:" ,count);
+
+console.log("============ b ============");
+
+var count=107;
+console.log("Total count is : ", count);
+
+var count="number";
+console.log("Total Count DataType After updating : ",count);
+
+
