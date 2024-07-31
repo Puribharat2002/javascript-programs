e5821aa80d1ccfa1bf6f80736ba6217e197a25fd
Puribharat2002 <bharatpuri7189@gmail.com>
2024-07-31 13:22:21 +0530

    variableAssign
    variableAssign

---
+++ 01-variableAssign.js
@@ -0,0 +1,28 @@
+console.log("============ 1 =============");
+var name;
+console.log("My Full  Name is before Initialization :", name);
+
+var firstName = "Bharat Puri";
+console.log("My First  & last Name  after  Initialization:", firstName);
+
+var fullName = "Bharat Sanjay Puri"
+console.log("My Full Name after Initialization:",fullName);
+
+console.log("============= 2 =================");
+console.log("I Love MySelf");
+
+console.log("===========3===============");
+var name = "Bharat";
+console.log("My First Name:", name);
+var lastName = "Puri";
+console.log("My Last Name:", lastName);
+var age = 22;
+console.log("My Age is:", age);
+var address = "L A";
+console.log("My Address is:",address);
+var pinCode = 445202;
+console.log("PinCode is:",pinCode);
+var gender = "Male";
+console.log("Gender:", gender);
+var marriage = "false";
+console.log("Marriage Status:",marriage);
