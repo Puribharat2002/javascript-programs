62c40a1669365cd9ce7c5491fbc6d5149515f6a1
Puribharat2002 <bharatpuri7189@gmail.com>
2024-07-31 07:54:05 +0530

    Javascript Variable
    Javascript Variable

---
+++ 01-variable-assignment.js
@@ -0,0 +1,4 @@
+console.log("my city name is Yavatmal");
+console.log("My Best friend name is Aniket");
+console.log("My School name is  Aided High School Darwha");
+console.log("My college name is PRMCEAM Badhnera");
