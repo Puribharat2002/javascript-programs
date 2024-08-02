400db4db16d84adb18ef103f5851a76ebc87adbe
Puribharat2002 <bharatpuri7189@gmail.com>
2024-08-02 19:22:03 +0530

    function.js
    function.js

---
+++ 03-funAssign.js
@@ -0,0 +1,29 @@
+console.log("=================== 1 =====================");
+
+function pearsonalDetailss(firstName , lastName){
+    console.log("Aniket Dudhe");
+    console.log("Gaurav Pasle");
+
+}
+pearsonalDetailss();
+
+console.log("================ 2 ========================");
+function pearsonalDetails(firstName , lastName , schoolName){
+    console.log("My First Name is : Bharat");
+    console.log("My Last Name is : Puri");
+    console.log("My School Name is : Shivaji School");
+
+}
+pearsonalDetails();
+
+console.log("=============== 3 ==================");
+
+function addition(n1, n2, n3){
+
+    console.log("Given numbers:", n1, n2 , n3);
+
+    var result = n1 + n2 +n3;
+
+    console.log("Addition is: ", result);
+}
+addition(10.23 , 600 , 40);
