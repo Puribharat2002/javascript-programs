f24fb71abbff615c03170267adde13fb8ade341b
Puribharat2002 <bharatpuri7189@gmail.com>
2024-08-06 22:24:18 +0530

    operator
    operator

---
+++ 05-operatorA.js
@@ -0,0 +1,50 @@
+function squareOfWordLength(sentence){
+
+    console.log(`Given sentence is: ${sentence}`);
+
+    var result = sentence.split(" ");
+
+    var len = result.length;
+
+    var square = len * len;
+
+    return square;
+
+}
+
+var result = squareOfWordLength("JavaScript");
+
+console.log(`Square of world length is: ${result}`);
+
+ 
+
+var result = squareOfWordLength("Google Chrome");
+
+console.log(`Square of world length is: ${result}`);
+
+ 
+
+var result = squareOfWordLength("Web Developer Smart");
+
+console.log(`Square of world length is: ${result}`);
+
+console.log("========================");
+
+var sentence = " I am angular developer ";
+ console.log(`Given string is : ${sentence}`);
+
+ var sentenceLength = sentence.length;
+
+console.log("String length is: ",sentenceLength);
+
+var sentenceLength = sentence.length;
+
+console.log("Total number of word is: 4  ");
+
+console.log("Dividing to string length by string word is: 5.5");
+console.log("multiplication is: 96");
+
+
+
+
+
