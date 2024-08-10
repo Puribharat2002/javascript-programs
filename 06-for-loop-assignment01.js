8a34cb5c09dafef00db31afbaa77a5a7532d45b9
Puribharat2002 <bharatpuri7189@gmail.com>
2024-08-10 12:59:20 +0530

    for-loop
    for-loop

---
+++ 06-for-loop-assignment01.js
@@ -0,0 +1,34 @@
+for (let i= 5; i<=15; i++) {
+    console.log(i);
+}
+
+console.log("==========print numbers from 50 to 40 decrement by 1=========== ");
+
+
+for (let index = 50; index >=40; index--) {
+   console.log(index);   
+}
+
+console.log("========== first 15 odd numbers ============");
+for (let index = 1; index <= 30; index=index+2) {
+    console.log(index);
+}
+console.log("========== first 10 even numbers ============");
+for (let index = 0; index <= 20; index=index+2) {
+    console.log(index);
+}
+
+console.log("========== table of 5 ============");
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+console.log("========== table of 10 ============");
+for (let index = 10; index <= 100; index=index+10) {
+    console.log(index);
+}
+
+console.log("========== table of 10 reverse ============");
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+}
